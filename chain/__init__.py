9fade458a17b05bd9c78a14686a4bab3e6a745ef
Muhammad Ali Bajwa <160342631+bajwa4200@users.noreply.github.com>
2025-04-01 09:42:15 +0500

    Optimize performance and refactor hot paths

---
+++ chain/__init__.py
@@ -0,0 +1,7 @@
+"""Mini blockchain package."""
+
+from chain.block import Block
+from chain.blockchain import Blockchain
+from chain.wallet import Wallet
+
+__all__ = ["Block", "Blockchain", "Wallet"]
